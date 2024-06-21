183d537975bcb9733bf4d991d6141a0ee5c9de8f
DiegoSM-on <diegoclashdiego@gmail.com>
2024-06-21 19:41:24 -0300

    Add files via upload

--- aula2.py
+++ aula2.py
@@ -1,15 +1,23 @@
-# Estrutura de repetição: FOR (PARA)
-# Repete uma quantidade finita de vezes
+#Coleção é um tipo de dado que permite armazenar um conjunto de valores.
 
-for i in range(100):
+#Lista é um tipo de coleção que permite inserir valores e acessar pelo indice.
+# Para definir uma lista colocamos []
+
+
+x: int = [15,12,1,"Diego",2,34]
+print(x)
+#Acesso pelo indice
+print("O valor no indice buscado é:",x[2:]) # ':' serve para separar os elementos da lista
+print("A quantidade de elemento na lista é", len(x)) #Verificar o tamanho da lista
+
+#Elementos na lista
+for i in x: # Serve para exibir os elementos na lista
     print(i)
 
 
+'''
+Crie uma lista ordenada em ordem crescente
+com 10 elementos inteiros. Depois crie uma forma
+de verificar se existe um número dentro da lista
+'''
 
-
-
-cont = 0 
-while cont <=100:
-    print(cont)
-    cont = cont + 1
-    
